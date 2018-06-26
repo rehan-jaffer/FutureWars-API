29d1902904c66c79fbf0935d280ff4d922e46871
Rehan Jaffer <ray@thelondonvandal.com>
2018-06-26 14:10:18 +0100

    isolated user auth / json web token functionality

---
+++ lib/user_auth_json.rb
@@ -0,0 +1,23 @@
+module UserAuthJson
+  private
+
+  attr_reader :headers
+
+  def user
+    @user ||= Player.find(decoded_auth_token[:player_id]) if decoded_auth_token
+    @user || errors.add(:token, 'Invalid token') && nil
+  end
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
+
+  def http_auth_header
+    if headers['Authorization'].present?
+      return headers['Authorization'].split(' ').last
+    else
+      errors.add(:token, 'Missing token')
+    end
+    nil
+  end
+end
