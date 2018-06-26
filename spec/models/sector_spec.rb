c1c61922b2e5dcd0e7935f6385016dd33fc65906
Rehan Jaffer <ray@thelondonvandal.com>
2018-06-26 08:58:28 +0100

    added failing tests for inbound and outbound methods on sector

--- spec/models/sector_spec.rb
+++ spec/models/sector_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Sector, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#inbound' do
+    before :all do
+      FactoryGirl.create(:sector_with_warps)
+    end
+    it 'returns the correct number of inbound warps' do
+      sector = Sector.last
+      expect(sector.inbound).to eq 3
+    end
+  end
+
+  describe '#outbound' do
+    before :all do
+      FactoryGirl.create(:sector_with_warps)
+    end
+    it 'returns the correct number of outbound warps' do
+      sector = Sector.last
+      expect(sector.outbound).to eq 3
+    end
+  end
 end
