192f000f523b791ffe7d9e69da15aa75b0eb637d
Rehan Jaffer <ray@thelondonvandal.com>
2018-06-26 14:06:16 +0100

    added can_move? call to Player class

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,16 @@
 class Player < ApplicationRecord
   has_secure_password
+
+  validates :ship_name, presence: true, format: /\A[a-zA-Z0-9 \-_]+\z/
+  validates :username, format: /\A[a-zA-Z0-9 ]+\z/
+
+  def can_move?(origin, dest)
+    return false unless turns > 0
+
+    return false unless Sector.is_connected?(origin, dest)
+
+    return false unless current_sector == origin
+
+    true
+  end
 end
