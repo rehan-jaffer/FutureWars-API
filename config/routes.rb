cf3dd2f7db297ba43db97ba06051fa0ccbb67571
Rehan Jaffer <ray@thelondonvandal.com>
2018-06-28 17:00:37 +0100

    allow api/sector#current route to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :auth
+    namespace :sector do
+      get 'current'
+    end
     namespace :player do
       get 'stats'
     end
