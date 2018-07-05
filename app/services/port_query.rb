1ab9fa71f8761cd7a8b0a4978b1908714fd6d364
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-05 09:20:10 +0100

    rubocop linted

--- app/services/port_query.rb
+++ app/services/port_query.rb
@@ -1,7 +1,6 @@
 require './lib/views/port_trade_view'
 
 class PortQuery
-
   prepend SimpleCommand
 
   def initialize(id)
@@ -9,12 +8,11 @@ class PortQuery
     # @user = user
   end
 
-  def call()
-    if !@id
-      errors.add(:auth, "No user supplied!")
+  def call
+    unless @id
+      errors.add(:auth, 'No user supplied!')
       return nil
     end
-    return PortTradeView.render(Sector.find(@id).attributes)
+    PortTradeView.render(Sector.find(@id).attributes)
   end
-
 end
