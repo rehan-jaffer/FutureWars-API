b082fb2906b9b37707a38e5b3e9edf39ff038058
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-05 14:18:19 +0100

    linting

--- lib/universe.rb
+++ lib/universe.rb
@@ -27,16 +27,15 @@ class Universe
   end
 
   def self.create(size, planet_ratio = 0.6, warp_function = nil)
-
     yield
 
     port_ratio = 0.9
     warp_function ||= ->(_x) { (rand * 5).round }
 
-    sector_list = (0..size).map { |i|
-        SectorCreator.call((rand < planet_ratio), (rand < port_ratio), $SECTORS[i]).result.id if $SECTORS[i]
-        SectorCreator.call((rand < planet_ratio), (rand < port_ratio)).result.id 
-    }
+    sector_list = (0..size).map do |i|
+      SectorCreator.call((rand < planet_ratio), (rand < port_ratio), $SECTORS[i]).result.id if $SECTORS[i]
+      SectorCreator.call((rand < planet_ratio), (rand < port_ratio)).result.id
+    end
 
     sector_list.each do |sector|
       Sector.create_warps(sector, sector_list, warp_function)
@@ -50,6 +49,5 @@ class Universe
       sector.federation_space = true
       sector.save
     end
-
   end
 end
