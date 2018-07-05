a8cee6a3cece7f0efeb767e6f318f21f6d2b71de
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-05 09:21:18 +0100

    refactored PortTradeString class to reuse PORT_STRINGS array, added reverse of get function

--- lib/port_trade.rb
+++ lib/port_trade.rb
@@ -1,14 +1,20 @@
 class PortTradeString
+  PORT_STRINGS = %w[Special BBS BSS BSB SBB SSB SBS BSS SSS BBB].freeze
+
   def self.get(port_class)
-    ['Special', 'BSS', 'BSB', 'SBB', 'SSB', 'SBS', 'BSS', 'SSS', 'BBB'][port_class]
+    PORT_STRINGS[port_class]
+  end
+
+  def self.get_class(port_string)
+    PORT_STRINGS.find_index(port_string)
   end
 end
 
 class PortTradeCommodity
   def self.buys_or_sells(port_class, commodity)
-    commodities = ["ore", "organics", "equipment"]
+    commodities = %w[ore organics equipment]
     return nil unless commodities.include?(commodity)
-    trades = PortTradeString.get(port_class).split("")[commodities.find_index(commodity)]
-    return trades == "B" ? "Buying" : "Selling" 
+    trades = PortTradeString.get(port_class).split('')[commodities.find_index(commodity)]
+    trades == 'B' ? 'Buying' : 'Selling'
   end
 end
