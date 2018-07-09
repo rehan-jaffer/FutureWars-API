0697d57a056ed6c1cade6d8160f02c39e34431f8
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-09 10:23:38 +0100

    removed 'players' prefix, rank should be supplied with the other fields

--- lib/views/player_view.rb
+++ lib/views/player_view.rb
@@ -1,14 +1,13 @@
 class PlayerView
     def self.render(attributes)
     players = Sector.find(attributes["id"]).players
-    {"players": players
-      .map { |player|
+    players.map { |player|
         {
          name: player.username,
+         rank: player.rank,
          fighters: player.fighters,
          ship: player.ship_name
         }
-      }
-    }
+     }
   end
 end
