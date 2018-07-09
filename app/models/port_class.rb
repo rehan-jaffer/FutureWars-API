191df3a0984c517fa392c16c9543483f86cc55d3
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-09 14:16:22 +0100

    added port_class value object

---
+++ app/models/port_class.rb
@@ -0,0 +1,20 @@
+class PortClass
+
+  PORT_TYPES = %w[Special BBS BSS BSB SBB SSB SBS BSS SSS BBB]
+  COMMODITIES = [:ore, :organics, :equipment]
+
+  def initialize(port_class)
+    @port_class = port_class
+  end
+
+  def to_s
+    PORT_TYPES[@port_class]
+  end
+
+  def trades?(trade_type, commodity)
+    choice = COMMODITIES.find_index(commodity)
+    return false unless choice
+    (trade_type == PORT_TYPES[@port_class].split("")[choice].to_sym)
+  end
+
+end
