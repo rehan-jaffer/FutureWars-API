5d1ae8b3aa845c9a94185f0099cf669c8c2a90e9
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-10 17:26:47 +0100

    streamlined sector creator service, removed planet and port creation responsibilities

--- app/services/sector_creator_service.rb
+++ app/services/sector_creator_service.rb
@@ -1,27 +1,14 @@
 class SectorCreatorService
   prepend SimpleCommand
 
-  def initialize(with_planet = true, with_port = true, additional_properties = {})
-    @with_planet = with_planet
-    @with_port = with_port
-    @additional_properties = additional_properties
+  def initialize(props = {})
+    @props = props
   end
 
   def call
-    sector_properties = @additional_properties.merge(has_port: false)
-    #    if @with_planet
-    #          sector_properties[:planet_type_id] = PlanetType.random_id
-    #          sector_properties[:planet_name] = PlanetNamer.generate_one
-    #    end
-    if @with_port
-      port_list = PortType.all.map(&:id)
-      sector_properties[:has_port] = true
-      sector_properties.merge!(generate_port)
-    end
-
-    Sector.find(sector_properties[:id]).destroy if sector_properties.key?(:id) && Sector.where(id: sector_properties[:id]).count > 0
-
-    new_sector = Sector.create(sector_properties)
+    Sector.find(@props[:id]).destroy if @props.key?(:id) && Sector.where(id: @props[:id]).count > 0
+    defaults = {}
+    new_sector = Sector.create(@props.merge(defaults))
     if new_sector.errors.empty?
       return new_sector
     else
@@ -30,19 +17,4 @@ class SectorCreatorService
     end
   end
 
-  private
-
-  def qty
-    1500 + (rand * 1000) - 500
-  end
-
-  def generate_port
-    {
-      port_class: ((rand * 7) + 1).round,
-      port_name: PlanetNamer.generate_one,
-      ore_qty: qty,
-      organics_qty: qty,
-      equipment_qty: qty
-    }
-  end
 end
