32d5eafda5ff992bdc2ae94e984c95642092cebd
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-10 15:04:48 +0100

    port_class object added as property, moved the logic out of this class into lib dir

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,27 +1,18 @@
 class Port
-
   # this is being re-used from lib/port_trade, remove duplication during refactoring
-  PORT_TYPES = %w[Special BBS BSS BSB SBB SSB SBS BSS SSS BBB]
-  COMMODITIES = [:ore, :organics, :equipment]
 
   def initialize(sector_attributes)
     @sector_attributes = sector_attributes
+    @port_class = PortClass.new(sector_attributes['port_class'])
   end
 
   # Nasty function. Add tests and then refactor this
   def trades?(buy_or_sell, commodity)
-
-    choice = COMMODITIES.find_index(commodity)
-    return false unless choice
-    (buy_or_sell == PORT_TYPES[@sector_attributes["port_class"]].split("")[choice].to_sym)
-
+    @port_class.trades?(buy_or_sell, commodity)
   end
 
-  def has_quantity?(commodity, qty)
+  def has_quantity?(_commodity, _qty)
     # placeholder function
     true
   end
-
 end
-
-
