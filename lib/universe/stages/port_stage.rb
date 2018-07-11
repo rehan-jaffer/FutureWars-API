ec42384151873bef592b74466fb16725aa05db55
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-11 17:46:05 +0100

    Removed MCIC references, no longer using this system. Cleaned up syntax"

--- lib/universe/stages/port_stage.rb
+++ lib/universe/stages/port_stage.rb
@@ -1,4 +1,4 @@
-require 'pp'
+require './lib/port_trade'
 
 class PortStage
   def initialize; end
@@ -8,24 +8,22 @@ class PortStage
   end
 
   def exec
-    # These are the distributions of the various port types in the original game as found online
- 
-    port_dist = {1 => 0.1999, 2 => 0.1991, 3 => 0.1999, 4 => 0.0997, 5 => 0.0997, 6 => 0.0997, 7 => 0.0496, 8 => 0.0522}
-    sector_list = Sector.pluck(:id)
-    ports = []
 
-    1000.times do |i|
-      z = port_dist.max_by { |_, weight| rand ** (1.0 / weight) }.first
-      ports.push(z)
+    # These are the distributions of the various port types in the original game
+
+    port_dist = { 1 => 0.1999, 2 => 0.1991, 3 => 0.1999, 4 => 0.0997, 5 => 0.0997, 6 => 0.0997, 7 => 0.0496, 8 => 0.0522 }
+
+    sector_list = Sector.pluck(:id)
+
+    ports = 0.upto(@size).map do |_i|
+      port_dist.max_by { |_, weight| rand**(1.0 / weight) }.first
     end
 
     ports.each do |port|
-      s = sector_list.delete_at(sector_list.length * rand)
-      Port.create!(port_class: port, sector_id: s, name: PlanetNamer.generate_one)
+      sector_props = { port_class: port, 
+                       sector_id: sector_list.delete_at(sector_list.length * rand), 
+                       name: PlanetNamer.generate_one }
+      Port.create!(sector_props)
     end
-
   end
-
-
-
 end
