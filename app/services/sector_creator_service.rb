ea6bc2967bd8dae6c9a88219171044601fb1ae1b
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-11 06:07:07 +0100

    refactored sector errors code

--- app/services/sector_creator_service.rb
+++ app/services/sector_creator_service.rb
@@ -8,13 +8,11 @@ class SectorCreatorService
   def call
     Sector.find(@props[:id]).destroy if @props.key?(:id) && Sector.where(id: @props[:id]).count > 0
     defaults = {}
-    new_sector = Sector.create(@props.merge(defaults))
-    if new_sector.errors.empty?
-      return new_sector
-    else
-      errors.add(:save, new_sector.errors)
-      return nil
-    end
-  end
 
+    new_sector = Sector.create(@props.merge(defaults))
+
+    return new_sector if new_sector.errors.empty?
+    errors.add(:save, new_sector.errors)
+    nil
+  end
 end
