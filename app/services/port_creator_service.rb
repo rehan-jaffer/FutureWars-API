7c3db00373694a59b405ad3679cbda0afbf8f4dd
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-11 17:53:24 +0100

    initial implementation of the port creator service

---
+++ app/services/port_creator_service.rb
@@ -0,0 +1,23 @@
+require './lib/port_trade'
+
+class PortCreatorService
+  prepend SimpleCommand
+
+  def initialize(props = {})
+    @props = props
+    if @props.key?(:trades)
+      @props['port_class'] = PortTradeString.get_class(@props[:trades])
+      @props.delete(:trades)
+    end
+  end
+
+  def call
+    @props['name'] = PlanetNamer.generate_one
+    port = Port.create(@props)
+    return port if port.errors.empty?
+    port.errors.full_messages.each do |error|
+      errors.add(:errors, error)
+    end
+    nil
+  end
+end
