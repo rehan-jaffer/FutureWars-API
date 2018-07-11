971b3033080384b4180dbc5f19cca908ec08ce7a
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-11 15:20:50 +0100

    Holds pricing algorithm implemented. Determines the base cost of the holds in a varying way using a modulus of the current day of the year. This can be customised further later

---
+++ lib/pricing/holds.rb
@@ -0,0 +1,9 @@
+module Holds
+
+  INCREMENT = 20 
+  BASE_COST = (DateTime.now.yday % 99) + 151
+
+  def self.price(number_holds)
+    (BASE_COST * number_holds) + (INCREMENT * number_holds) * (number_holds - 1) / 2
+  end
+end
