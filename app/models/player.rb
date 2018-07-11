1916bc4599fd941afaa3d640ca28647cfbf93c95
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-11 06:07:39 +0100

    added ship_type association, allow viewing of ship_type name (used in player views)

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,14 +1,18 @@
 class Player < ApplicationRecord
   has_secure_password
 
-  has_one :ship_type
-
   validates :ship_name, presence: true, format: /\A[a-zA-Z0-9 \-_]+\z/
   validates :username, format: /\A[a-zA-Z0-9 ]+\z/, uniqueness: true
+
+  belongs_to :ship_type
 
   def can_trade_at_port?(_sector_id)
     # until implemented, players can trade at all ports
     true
+  end
+
+  def ship_type_name
+    ship_type.name
   end
 
   def ship
