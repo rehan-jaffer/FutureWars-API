0f40784b65f1919515e5b5aaa4b7bf210c790c9d
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-13 16:20:25 +0100

    updated port model to contain functionality in diverse classes before (portclass etc.) and simplify organisation

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,18 +1,22 @@
 class Port < ApplicationRecord
   # this is being re-used from lib/port_trade, remove duplication during refactoring
+  PORT_TYPES = %w[Special BBS BSS BSB SBB SSB SBS BSS SSS BBB].freeze
+  COMMODITIES = %w[ore organics equipment].freeze
+
   belongs_to :sector
-
-  def initialize
-    @port_class = PortClass.new(port_class)
-  end
-
-  # Nasty function. Add tests and then refactor this
-  def trades?(buy_or_sell, commodity)
-    @port_class.trades?(buy_or_sell, commodity)
-  end
 
   def has_quantity?(_commodity, _qty)
     # placeholder function
     true
+  end
+
+  def to_s
+    PORT_TYPES[port_class]
+  end
+
+  def trades?(trade_type, commodity)
+    choice = COMMODITIES.find_index(commodity)
+    return false unless choice
+    (trade_type[0].upcase == PORT_TYPES[port_class].split('')[choice])
   end
 end
