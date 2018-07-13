139bdbb3b20e2b95186db69225d66d178b4e7b65
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-13 19:11:20 +0100

    Refactored to use a TradingStrategy implementation specified in a config file (config/initializers/trading_strategy.rb). This is an extremely simplified version, but implements the kind of modular approach that will allow swappable trading strategies for ports

--- app/services/consider_offer_service.rb
+++ app/services/consider_offer_service.rb
@@ -1,35 +1,36 @@
 class ConsiderOfferService
-
   prepend SimpleCommand
-  
+
   TARGET_PRICE = 20
 
   def initialize(player, transaction_id, amount)
-    @transaction = Transaction.find(transaction_id)
+    @transaction = Transaction.where(id: transaction_id).first
     @player = player
     @amount = amount
   end
 
   def call
     errors.add(:errors, "Transaction doesn't exist") unless @transaction
-    errors.add(:errors, "This isn't your transaction") unless @transaction.player_id == @player.id
-    errors.add(:errors, "This transaction has been closed") unless @transaction.status == "open"
+    errors.add(:errors, "This isn't your transaction") unless @transaction && @transaction.player_id == @player.id
+    errors.add(:errors, 'This transaction has been closed') unless @transaction && @transaction.status == 'open'
 
     return nil unless errors.empty?
 
-    if @amount == TARGET_PRICE
-      @transaction.status = "accepted"
+    strategy = Rails.configuration.trading_strategy
+
+    unless strategy.will_negotiate?(@amount)
+      @transaction.status = 'rejected'
       @transaction.save
-      return @transaction
-    elsif @amount > TARGET_PRICE
-      @offer_amount = (TARGET_PRICE + (@amount-TARGET_PRICE)/2) 
-      Offer.create(transaction_id: @transaction.id, amount: @offer_amount)
-    elsif @amount < TARGET_PRICE
-      @transaction.status = "rejected"
-      @transaction.save
-      errors.add(:errors, "Transaction terminated.")
+      errors.add(:errors, 'Transaction terminated.')
       return nil
     end
-  end
 
+    if strategy.will_accept?(@amount)
+      @transaction.status = 'accepted'
+      @transaction.save
+      return @transaction
+    end
+
+    Offer.create(transaction_id: @transaction.id, amount: strategy.counter_offer(@amount))
+  end
 end
