38ef428d2bbcd6db03b84fcb68a2805a4a3e3101
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-14 11:04:46 +0100

    transactions#offer now implemented, uses considerofferservice.call

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,7 +1,15 @@
 class Api::TransactionsController < ApplicationController
-
   def offer
+    result = ConsiderOfferService.call(current_user, offer_params[:id], offer_params[:amount])
+    if result.success?
+      render json: result.result
+    else
+      render json: result.errors
+    end
+  end
 
+  def offer_params
+    params.permit(:id, :amount)
   end
 
 end
