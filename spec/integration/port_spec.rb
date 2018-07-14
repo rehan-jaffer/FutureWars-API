fc36e9901627d53e8219a832dbac93a73f59ff48
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-14 16:18:26 +0100

    added more tests for port trading transactions. Three types now added: high offer, low-ball offer that pisses off the trader, and optimal offer, via the DummyStrategy trading strategy

--- spec/integration/port_spec.rb
+++ spec/integration/port_spec.rb
@@ -14,6 +14,10 @@ describe 'Port Trading Functionality' do
     @auth = authenticate_user('ray', 'testpassword')
   end
 
+  after :all do
+    Player.destroy_all
+  end
+
   describe 'Querying a Port for trading information' do
     it 'allows querying of a port' do
       get '/api/ports/query/2', headers: { 'AUTHORIZATION': @auth['auth_token'] }
@@ -45,13 +49,56 @@ describe 'Port Trading Functionality' do
   end
 
   describe 'Trading with a port (opening a transaction)' do
-    it 'permits trading with a port' do
-      player = Player.find_by(username: 'ray')
-      player.current_sector = 2
-      player.save
+ 
+   before :all do
+     @player = Player.find_by_username("ray")
+   end
+
+   it 'permits trading with a port and opening a transaction' do
+      @player.current_sector = 2
+      @player.save
 
       post '/api/ports/trade', params: { id: 2, qty: 1, commodity: 'equipment', buy_or_sell: 'buy' }, headers: { 'AUTHORIZATION': @auth['auth_token'] }
-      pp JSON.parse(response.body)
+      transaction = JSON.parse(response.body)
+      expect(transaction).to have_key("transaction")
+      expect(transaction).to have_key("initial_offer")
     end
+
   end
+
+  describe "Transaction handling (Dummy Trading Strategy)" do
+
+    before :all do
+      @player = Player.find_by_username("ray")
+      @player.current_sector = 2
+      @player.save
+
+      @transaction = Transaction.create(player_id: @player.id, port_id: Sector.find(@player.current_sector).port.id, status: "open")
+      Rails.configuration.trading_strategy = DummyStrategy
+    end
+
+    it "permits making a counteroffer to a port (high offer) and receives a counteroffer in return" do
+      offer_amount = 25
+      post '/api/transactions/offer', params: { id: @transaction.uid, amount: offer_amount }, headers: {'AUTHORIZATION': @auth["auth_token"]}
+      transaction = JSON.parse(response.body)
+      expect(transaction).to have_key("offer")
+      expect(transaction["offer"]["amount"]).to be < offer_amount
+    end
+
+    it "permits making a counteroffer to a port (low offer) and receives a transaction termination in return" do
+      offer_amount = 15
+      post '/api/transactions/offer', params: { id: @transaction.uid, amount: offer_amount }, headers: {'AUTHORIZATION': @auth["auth_token"]}
+      transaction = JSON.parse(response.body)
+      expect(transaction).to have_key("errors")
+    end
+
+    it "permits making a counteroffer to a port (optimal offer) which is accepted" do
+      offer_amount = 20
+      post '/api/transactions/offer', params: { id: @transaction.uid, amount: offer_amount }, headers: {'AUTHORIZATION': @auth["auth_token"]}
+      transaction = JSON.parse(response.body)
+      expect(transaction).to have_key("transaction")
+    end
+
+  end
+
 end
