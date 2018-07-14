8b127d77078fd02ba805b9996b59df703deee6a4
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-14 11:05:31 +0100

    added ID Generator for creating Unique(ish) IDs

---
+++ lib/generators/id_generator.rb
@@ -0,0 +1,9 @@
+class IDGenerator
+  def self.generate_id(n)
+    1.upto(n).map { |_| ((0..9).to_a + ('A'..'Z').to_a).sample }.join
+  end
+
+  def self.generate_serial
+    "SCN00-" + IDGenerator.generate_id(16)
+  end
+end
