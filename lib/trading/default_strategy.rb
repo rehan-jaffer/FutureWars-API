42bcb727072364c3c591752a5fe1d0a00f77afd1
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-17 09:48:45 +0100

    DefaultStrategy

--- lib/trading/default_strategy.rb
+++ lib/trading/default_strategy.rb
@@ -5,15 +5,15 @@ class DefaultStrategy
   end
 
   def self.will_negotiate?(offer_data)
-    (amount >= 20)
+    (offer_data.amount >= 20)
   end
 
   def self.will_accept?(offer_data)
-    (amount == 20)
+    (offer_data.amount == 20)
   end
 
   def self.counter_offer(offer_data)
-    FIXED_PRICE + ((amount - FIXED_PRICE) / 2)
+    FIXED_PRICE + ((offer_data.amount - FIXED_PRICE) / 2)
   end
 
 end
