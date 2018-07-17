6711f2d289ff23e77e2d5d3944746f30d0db7cc2
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-17 09:51:50 +0100

    renamed auth path in routes to session

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,26 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :auth
+
+    resources :session
+
     namespace :sector do
       get 'current'
       get 'view/:id', action: :view
     end
+
     namespace :player do
       get 'stats'
       post 'move'
     end
+
     namespace :ports do
       get 'query/:id', action: :query
       post 'trade'
     end
+
     namespace :transactions do
       post 'offer'
     end
+
   end
 end
