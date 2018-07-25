37d91d308a99d1941426332f2db02b23fca34ba7
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-25 14:49:46 +0100

    converted to use a plain hash rather than a fancy OfferData object

--- lib/trading/default_strategy.rb
+++ lib/trading/default_strategy.rb
@@ -4,28 +4,26 @@ require 'pp'
 class DefaultStrategy
 
   def self.initial_offer_price(offer_data)
-    commodity = (offer_data.transaction) ? offer_data.transaction["commodity"] : offer_data.commodity
-    mcic = offer_data.port["#{commodity}_mcic"]
-    trading_percent = offer_data.port["#{commodity}_trading"]
-    Pricing::Fuel_Ore::per_unit_price2(mcic, trading_percent)*qty(offer_data)
+    port = Port.find(offer_data[:port_id])
+    port.per_unit_price(offer_data[:commodity]) * offer_data[:qty].to_i
   end
 
   def self.will_negotiate?(offer_data)
-    offer_data.amount < (offer_data.transaction["initial_offer"]) && offer_data.amount > (offer_data.transaction["initial_offer"] * 0.6)
+    offer_data[:amount] < (offer_data[:initial_offer]) && offer_data[:amount] > (offer_data[:initial_offer] * 0.6)
   end
 
   def self.will_accept?(offer_data)
-    (offer_data.amount < offer_data.transaction["initial_offer"]) && offer_data.amount > (offer_data.transaction["initial_offer"] * 0.9)
+    (offer_data[:amount] < offer_data[:initial_offer]) && offer_data[:amount] > (offer_data[:initial_offer] * 0.9)
   end
 
   def self.counter_offer(offer_data)
-    offer_data.amount - (offer_data.amount - initial_offer_price(offer_data)).abs
+    offer_data[:amount] - (offer_data[:amount] - initial_offer_price(offer_data)).abs
   end
 
   private
 
     def self.qty(offer_data)
-     (offer_data.qty.to_i || offer_data.transaction["qty"])
+     (offer_data.qty.to_i || offer_data[:qtys])
     end
 
 end
