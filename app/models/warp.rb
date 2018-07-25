81a76d875267eb4d0feb1993f9723734608a7ca3
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-25 14:48:53 +0100

    connect method moved into Warp class to avoid Sector knowing internals. Same for warps_for, which is a replacement for Sector.warps

--- app/models/warp.rb
+++ app/models/warp.rb
@@ -5,4 +5,17 @@ class Warp < ApplicationRecord
         .or(Warp.where(origin_id: dest, dest_id: origin)).count > 0
     end
 
+    def self.connect(orig, dest)
+      Warp.create!(origin_id: orig, dest_id: dest)
+    end
+
+    def self.warps_for(id)
+      Warp.where(origin_id: id)
+          .or(Warp.where(dest_id: id))
+          .map { |el| [el[:origin_id], el[:dest_id]] }
+          .flatten
+          .uniq
+          .reject { |x| x == id }
+    end
+
 end
