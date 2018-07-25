09feef29960b306f25f69a066004886c9bb90748
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-25 14:51:42 +0100

    added validations, offers association, and current_offer

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,13 +3,30 @@ require './lib/generators/id_generator'
 class Transaction < ApplicationRecord
   belongs_to :player
   belongs_to :port
+  has_many :offers
   before_create :set_uid
+
+  validates :initial_offer, presence: true
+  validates :trade_type, presence: true
+  validates :qty, presence: true, numericality: true
+  validates_associated :port
 
   def set_uid
     self.uid = IDGenerator.generate_serial
   end
 
   def as_json(_opts = {})
-    { uid: uid, status: status, age: ((Time.now - created_at) / 60.0).round }
+    { uid: uid, status: status, age: age_in_minutes }
   end
+
+  def current_offer
+    offers.last
+  end
+
+  private
+
+    def age_in_minutes
+      ((Time.now - created_at) / 60.0).round
+    end
+
 end
