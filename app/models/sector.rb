089cbb2b0f76f65f55c4395c2a390021eb23627c
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-25 14:51:15 +0100

    Warp class now contains connect

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -30,34 +30,12 @@ class Sector < ApplicationRecord
   end
 
   def warps
-    Warp.where(origin_id: id)
-        .or(Warp.where(dest_id: id))
-        .map { |el| [el[:origin_id], el[:dest_id]] }
-        .flatten
-        .uniq
-        .reject { |x| x == id }
+    Warp.warps_for(id)
   end
 
-  def self.warps(id)
-    Warp.where(origin_id: id)
-        .or(Warp.where(dest_id: id))
-        .map { |el| [el[:origin_id], el[:dest_id]] }
-        .flatten
-        .uniq
-        .reject { |x| x == id }
-  end
-
-  def self.connect(orig, dest)
-    Warp.create!(origin_id: orig, dest_id: dest)
-  end
-
-  def self.connected?(origin, dest)
-    Warp.where(origin_id: origin, dest_id: dest)
-        .or(Warp.where(origin_id: dest, dest_id: origin)).count > 0
-  end
 
   def self.create_warps(id, id_list, _max_warps = 5, warp_function)
-   warp_function.call(0).times { |_| connect(id, id_list[rand * id_list.size]) }
+   warp_function.call(0).times { |_| Warp.connect(id, id_list[rand * id_list.size]) }
   end
 
   private 
