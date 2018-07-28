2aac60ddc1da8568d588fd89ca3f68e6ed205136
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-28 05:45:38 +0100

    initial implementation of the path finder service, provides information on plotting a path through space

---
+++ app/services/path_finder_service.rb
@@ -0,0 +1,20 @@
+class PathFinderService
+  prepend SimpleCommand
+
+  def initialize(player_id, dest)
+    @player = Player.find(player_id)
+    @dest = dest
+  end
+
+  def validates?
+    errors.add(:errors, "Destination does not exist!") unless Sector.exists?(@dest)
+    errors.empty?
+  end
+
+  def call
+    return nil unless validates?
+    path = Warp.path(@player.current_sector, @dest)
+    {path: path, nodes: path.size, cost: @player.move_cost(path.size), can_warp: @player.can_express_warp?(@dest)}
+  end
+
+end
