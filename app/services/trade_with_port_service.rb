9142c0ba04cd05c5c53d376b059bee38d12cf89b
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-30 08:48:43 +0100

    rubocop-related changes

--- app/services/trade_with_port_service.rb
+++ app/services/trade_with_port_service.rb
@@ -6,7 +6,7 @@ class TradeWithPortService
     @player = Player.find(player_id)
     @sector = Sector.find(@player.current_sector)
     @strategy = Rails.configuration.trading_strategy
-    @offer_data = {port_id: @sector.port.id}.merge(@request)
+    @offer_data = { port_id: @sector.port.id }.merge(@request)
   end
 
   def validates?
@@ -25,12 +25,11 @@ class TradeWithPortService
   end
 
   def call
-
     return errors unless validates?
 
     offer_price = @strategy.initial_offer_price(@offer_data)
 
-    transaction = Transaction.create(@request.merge({player_id: @player.id, port_id: @sector.port.id, open: true, status: :initial, initial_offer: offer_price}))
+    transaction = Transaction.create(@request.merge(player_id: @player.id, port_id: @sector.port.id, open: true, status: :initial, initial_offer: offer_price))
     offer = Offer.create(transaction_id: transaction.id, amount: offer_price)
 
     { transaction: transaction, initial_offer: offer }
