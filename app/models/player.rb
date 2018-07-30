4946f4c710aeaa9d9c4e2393b5f0b32ffa0a4835
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-30 18:47:35 +0100

    added validations for username, current_sector and alignment. has_events? placeholder function added

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,31 @@
+require './lib/player/turns'
+
 class Player < ApplicationRecord
+  include PlayerUnit::Turns
+
   has_secure_password
 
+  has_one :ship
+
   validates :ship_name, presence: true, format: /\A[a-zA-Z0-9 \-_]+\z/
-  validates :username, format: /\A[a-zA-Z0-9 ]+\z/, uniqueness: true
+  validates :username, format: /\A[a-zA-Z0-9 ]+\z/, uniqueness: true, presence: true
+  validates :current_sector, presence: true
+  validates :alignment, presence: true
+  
 
   belongs_to :ship_type
+
+  def update_sector(sector_id)
+    update_attribute(:current_sector, sector_id)
+  end
+
+  def has_events?
+    false
+  end
+
+  def can_express_warp?(dest)
+    move_cost( Warp.path(current_sector, dest).size ) < turns
+  end
 
   def rank
     @player_rank ||= PlayerRank.new(exp, alignment.to_i).to_s
@@ -16,9 +37,9 @@ class Player < ApplicationRecord
   end
 
   delegate :name, to: :ship_type, prefix: true
+  delegate :turns_per_warp, to: :ship_type, prefix: true
 
-  def ship
-    @ship ||= Ship.new
-  end
-
+  #  def ship
+  #    @ship ||= Ship.new
+  #  end
 end
