320d2233dba3f49fcbc6cf97a1db24dc1fc493c4
Rehan Jaffer <ray@thelondonvandal.com>
2018-07-30 18:43:45 +0100

    offers should have a parent transaction. added final? to indicate this is a final offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,6 @@
 class Offer < ApplicationRecord
   before_create :set_uid
+  has_one :owner, foreign_key: 'transaction_id', class_name: 'Transaction'
 
   def set_uid
     self.uid = IDGenerator.generate_serial
@@ -7,5 +8,9 @@ class Offer < ApplicationRecord
 
   def as_json(_opts = {})
     { id: uid, amount: amount }
+  end
+
+  def final?
+    final
   end
 end
