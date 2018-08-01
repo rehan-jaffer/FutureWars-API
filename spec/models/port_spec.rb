d3e14d4b29a9d8270b4768649903d8a0bb20d5ad
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-01 19:00:59 +0100

    rubocop linting

--- spec/models/port_spec.rb
+++ spec/models/port_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Port, type: :model do
+  describe '.to_s' do
+    let(:sector) { FactoryBot.create(:sector) }
+    let(:port) { FactoryBot.create(:port, port_class: 2, sector_id: sector.id) }
 
-    describe ".to_s" do
-      let(:sector) { FactoryBot.create(:sector) }
-      let(:port) { FactoryBot.create(:port, port_class: 2, sector_id: sector.id) }
-
-      context "when class 0" do
-        it "stringifies as 'Special'" do
-          port.port_class = 0
-          expect(port.to_s).to eq 'Special'
-        end
+    context 'when class 0' do
+      it "stringifies as 'Special'" do
+        port.port_class = 0
+        expect(port.to_s).to eq 'Special'
       end
-
-      context "when class 2" do
-        it "stringifies as 'BBS'" do
-          port.port_class = 1
-          expect(port.to_s).to eq 'BBS'
-        end
-      end
-
     end
 
+    context 'when class 2' do
+      it "stringifies as 'BBS'" do
+        port.port_class = 1
+        expect(port.to_s).to eq 'BBS'
+      end
+    end
+  end
 end
