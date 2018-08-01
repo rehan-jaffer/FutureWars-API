50d6e4a012948cc60875a52f0daecc1e8f65dc97
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-01 18:57:54 +0100

    initial scheduler code used in config/schedule.rb

---
+++ lib/scheduler/scheduler.rb
@@ -0,0 +1,19 @@
+module Scheduler
+  module Ports
+    def self.update_quantities
+    end
+  end
+
+  module Players
+    def self.update_turns
+      turns = (Rails.configuration.game['turns_per_day'] / 24)
+      turns_per_day = Rails.configuration.game['turns_per_day']
+      Player.update_all(["turns = case
+                                    when turns + ? >= ? then
+                                      300
+                                    when turns + ? < ? then
+                                      turns + ?
+                                    end", turns, turns_per_day, turns, turns_per_day, turns])
+    end
+  end
+end
