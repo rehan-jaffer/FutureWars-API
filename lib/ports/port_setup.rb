4dccdcdf60bd2159e9c4bddaaf5f58660480980f
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-01 19:01:44 +0100

    rubocop linting

--- lib/ports/port_setup.rb
+++ lib/ports/port_setup.rb
@@ -6,15 +6,12 @@ class PortSetup
   end
 
   def self.get_mcic(commodity, trade_type)
-
-    commodity_ranges = {'ore': (40..90), 'organics': (30..75), 'equipment': (20..65)}  
-    commodity_ranges[commodity.to_sym].to_a.sample * ((trade_type == "Buying") ? -1 : 1)
-
+    commodity_ranges = { 'ore': (40..90), 'organics': (30..75), 'equipment': (20..65) }
+    commodity_ranges[commodity.to_sym].to_a.sample * (trade_type == 'Buying' ? -1 : 1)
   end
 
   def self.initialize(port)
-
-    commodities = ['ore', 'organics', 'equipment']
+    commodities = %w[ore organics equipment]
 
     commodities.each do |commodity|
       port["#{commodity}_mcic"] = get_mcic(commodity, PortTradeCommodity.buys_or_sells(port.port_class, commodity))
@@ -29,6 +26,5 @@ class PortSetup
     port.equipment_qty = port.equipment_productivity * 10
 
     port.save
-  
   end
 end
