b7615e1d4ada5267505e068107ce20952528deea
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-05 09:44:28 +0100

    moved sector controller into nav namespace

---
+++ app/controllers/api/nav/sector_controller.rb
@@ -0,0 +1,11 @@
+class Api::Nav::SectorController < ApplicationController
+  skip_before_action :authenticate_request, only: [:view]
+
+  def current
+    render json: Sector.find(current_user.current_sector).view
+  end
+
+  def view
+    render json: Sector.find(params[:id])
+  end
+end
