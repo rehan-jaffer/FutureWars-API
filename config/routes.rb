50c34ead9d5b9ea13171b16b5d82e7f6ea09b11b
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-06 17:37:15 +0100

    sector moved into nav namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,23 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     resources :sessions
 
-    namespace :sector do
-      get 'current'
-      get 'view/:id', action: :view
-    end
-
     namespace :player do
       get 'stats'
       post 'move'
     end
 
-    namespace :nav do
+   namespace :nav do
+   
       post 'warp'
       get 'warp_path'
       post 'express_warp'
-    end
+
+      namespace :sector do
+        get 'current'
+        get 'view/:id', action: :view
+      end
+
+   end
 
    namespace :subspace do
 
@@ -31,13 +33,5 @@ Rails.application.routes.draw do
 
    end
 
-    namespace :ports do
-      get 'query/:id', action: :query
-      post 'trade'
-    end
-
-    namespace :transactions do
-      post 'offer'
-    end
   end
 end
