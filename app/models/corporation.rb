8661b44cef74b6bebb22f7a560fc5df9134dfb80
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-09 16:07:09 +0100

    corporation name should be unique

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,6 +1,9 @@
 class Corporation < ApplicationRecord
+
   has_one :ceo, foreign_key: "ceo_id", class_name: "Player"
   has_many :members, foreign_key: "corporation_id", class_name: "Player"
+
+  validates :name, uniqueness: { case_sensitive: true }
 
   scope :with_ceo, -> (id) { where(ceo_id: id) }  
 
