81fafa51c7f24dec06df77926bb193e8865aa897
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-09 16:05:06 +0100

    new events

--- config/initializers/events.rb
+++ config/initializers/events.rb
@@ -1,3 +1,7 @@
+require './lib/subscribers/experience_tracker'
+require './lib/subscribers/promotions_tracker'
+require './lib/subscribers/event_logger'
+
 PlayerCreated = Class.new(RailsEventStore::Event)
 PlayerMoved = Class.new(RailsEventStore::Event)
 PlayerDestroyed = Class.new(RailsEventStore::Event)
@@ -11,3 +15,8 @@ PlayerBoughtFighters = Class.new(RailsEventStore::Event)
 PlayerBoughtShieldPoints = Class.new(RailsEventStore::Event)
 TradeRejected = Class.new(RailsEventStore::Event)
 TradeAccepted = Class.new(RailsEventStore::Event)
+CorporationCreated = Class.new(RailsEventStore::Event)
+PlayerJoinedCorporation = Class.new(RailsEventStore::Event)
+PlayerLeftcorporation = Class.new(RailsEventStore::Event)
+PlayerGainedExperience = Class.new(RailsEventStore::Event)
+PlayerPromoted = Class.new(RailsEventStore::Event)
