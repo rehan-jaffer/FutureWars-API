00918ea8648cd5831fba59c866eeb85f098f14d3
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-09 16:07:29 +0100

    rankings code initial commit

---
+++ lib/rankings.rb
@@ -0,0 +1,39 @@
+class Rankings
+
+  def self.experience_levels
+    0.upto(22).map { |n| 2**(n) }
+  end
+
+  def self.experience_level(exp)
+    experience_levels.find_index { |n| n >= exp }
+  end
+
+  def self.rank(exp)
+    good_ranks[experience_level(exp)]
+  end
+
+  def self.good_ranks
+    ["Private", 
+     "Private 1st Class", 
+     "Lance Corporal", 
+     "Corporal", 
+     "Sergeant", 
+     "Staff Sergeant", 
+     "Gunnery Sergeant", 
+     "1st Sergeant", 
+     "Sergeant Major", 
+     "Warrant Officer", 
+     "Chief Warrant Officer", 
+     "Ensign", 
+     "Lieutenant J.G", 
+     "Lieutenant", 
+     "Lieutenant Commander", 
+     "Commander", 
+     "Captain", 
+     "Commodore", 
+     "Rear Admiral", 
+     "Vice Admiral", 
+     "Admiral", 
+     "Fleet Admiral"]
+  end
+end
