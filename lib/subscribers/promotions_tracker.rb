763840ee1ca41dd20d2039f02846b803b0e21c56
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-10 06:07:12 +0100

    oops, forgot to add stream name

--- lib/subscribers/promotions_tracker.rb
+++ lib/subscribers/promotions_tracker.rb
@@ -6,7 +6,7 @@ class PromotionsTracker
     exp_gain = event.data[:exp]
     player = Player.find(event.data[:player_id])
     if Rankings.rank(player.exp - 2) != Rankings.rank(player.exp)
-      event_store.publish(PlayerPromoted.new(data: {player_id: player.id, new_rank: Rankings.rank(player.exp)}))
+      event_store.publish(PlayerPromoted.new(data: {player_id: player.id, new_rank: Rankings.rank(player.exp)}), stream_name: "player-#{player.id}")
     end
   end
 
