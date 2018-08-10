b5d036545b30dda03d82407c9dd2dfe32f627ea3
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-10 15:45:39 +0100

    removed code no longer in use

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,10 +1,4 @@
 require 'port_trade'
-require 'views/sector_view'
-require 'views/player_view'
-require 'views/port_view'
-require 'views/beacon_view'
-require 'views/planet_view'
-require 'views/warp_view'
 
 class Sector < ApplicationRecord
 
@@ -19,12 +13,6 @@ class Sector < ApplicationRecord
     !port.nil?
   end
 
-  def view
-    objects.map.with_object({}) do |object, h|
-      h[object] = handlers[object].render(attributes)
-    end
-  end
-
   def players_in_sector
     Player.where(current_sector: id).all
   end
@@ -33,17 +21,4 @@ class Sector < ApplicationRecord
     Warp.warps_for(id)
   end
 
-  def self.create_warps(id, id_list, _max_warps = 5, warp_function)
-    warp_function.call(0).times { |_| Warp.connect(id, id_list[rand * id_list.size]) }
-  end
-
-  private
-
-  def objects
-    %i[players beacons port planets warps sector]
-  end
-
-  def handlers
-    { players: PlayerView, beacons: BeaconView, port: PortView, planets: PlanetView, warps: WarpView, sector: SectorView }
-  end
 end
