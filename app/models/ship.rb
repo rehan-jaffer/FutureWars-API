4af21158251b8c7cfc463de237f46aeb0275704a
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-10 14:16:05 +0100

    refactored ship class to read a lot clearer

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,4 @@
 class Ship < ApplicationRecord
-
   belongs_to :player
   has_many :holds
   belongs_to :ship_type
@@ -9,23 +8,23 @@ class Ship < ApplicationRecord
   end
 
   def fighter_capacity
-    (ship_type.max_fighters - fighters)
+    ship_type.max_fighters - fighters
   end
 
   def shield_capacity
-    (ship_type.max_shields - shields)
+    ship_type.max_shields - shields
   end
 
-  def available_holds
-    (ship_type.base_holds + additional_holds)
+  def total_holds
+    ship_type.base_holds + additional_holds
   end
 
   def empty_holds
-    available_holds - holds.map { |hold| hold.qty }.inject(:+).to_i
+    total_holds - holds_in_use
   end
 
   def hold_contents
-    holds.each_with_object(Hash.new(0)) { |hold, hsh| hsh[hold.contents] += hold.qty }
+    holds.each_with_object(Hash.new(0)) { |hold, h| h[hold.contents] += hold.qty }
   end
 
   def have_quantity?(commodity, qty)
@@ -33,26 +32,45 @@ class Ship < ApplicationRecord
   end
 
   def load_hold(commodity, qty)
-    return false unless commodities.include?(commodity)
-    return false unless available_holds > 0 && empty_holds > 0
 
-    if have_quantity?(commodity, 1)
-      holds.where(contents: commodity).first.increment!(:qty, qty) if have_quantity?(commodity, 1)
-    else
-      Hold.create(contents: commodity, qty: qty, ship_id: id)
-    end
+    return false unless valid_commodity?(commodity)
+    return false unless total_holds > 0 && empty_holds > 0
+
+    have_quantity?(commodity, 1) ? increase_qty(commodity, qty) : create_qty(commodity, qty)
 
     reload
   end
- 
+
   def jettison(commodity, qty)
-    return false unless commodities.include?(commodity)
+    return false unless valid_commodity?(commodity)
     return false unless have_quantity?(commodity, qty)
-    holds.where(contents: commodity).first.decrement!(:qty, qty)  
+
+    decrease_qty(commodity, qty)
+  end
+
+  private
+
+  def increase_qty(commodity, qty)
+    holds.where(contents: commodity).first.increment!(:qty, qty)
+  end
+
+  def decrease_qty(commodity, qty)
+    holds.where(contents: commodity).first.decrement!(:qty, qty)
+  end
+
+  def create_qty(commodity, qty)
+    Hold.create(contents: commodity, qty: qty, ship_id: id)
+  end
+
+  def holds_in_use
+    holds.map(&:qty).inject(:+).to_i
+  end
+
+  def valid_commodity?(commodity)
+    commodities.include?(commodity)
   end
 
   def commodities
-    [:equipment, :ore, :organics, :colonists]
+    %i[equipment ore organics colonists]
   end
-
 end
