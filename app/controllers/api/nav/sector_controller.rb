dc2daf0ab2a6b01488b9141ceb07edacc946b796
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-11 05:55:41 +0100

    no longer using manually built views, removed call

--- app/controllers/api/nav/sector_controller.rb
+++ app/controllers/api/nav/sector_controller.rb
@@ -2,7 +2,7 @@ class Api::Nav::SectorController < ApplicationController
   skip_before_action :authenticate_request, only: [:view]
 
   def current
-    render json: Sector.find(current_user.current_sector).view
+    render json: Sector.find(current_user.current_sector)
   end
 
   def view
