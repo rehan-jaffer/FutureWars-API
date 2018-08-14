606e778e25ba423ae978f7dc6cedcf2ae4810850
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-14 08:15:26 +0100

    removed SectorView, no longer in use

--- app/controllers/api/player_controller.rb
+++ app/controllers/api/player_controller.rb
@@ -10,7 +10,7 @@ class Api::PlayerController < ApplicationController
   def move
     result = MovePlayerService.call(current_user.id, player_params[:id].to_i)
     if result.success?
-      render json: SectorView.render(Sector.find(current_user.current_sector))
+      render json: Sector.find(current_user.current_sector)
     else
       render json: result.errors, status: :forbidden
     end
