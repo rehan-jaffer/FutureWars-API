cc7aa4f3c3b0ca96303127c84515e825c6fea6e4
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 06:05:23 +0100

    fixed some issues with joincorporationservice, @user should have been @player in the event update, check corporation exists with Corporation.exists?

--- app/services/join_corporation_service.rb
+++ app/services/join_corporation_service.rb
@@ -7,23 +7,26 @@ class JoinCorporationService
 
   def initialize(user_id, corp_id)
     @player = Player.find(user_id)
-    @corp = Corporation.find(corp_id)
+    @corp_id = corp_id
     streams(["universe", "corporations", "corporation-#{corp_id}"])
   end
 
   def validates?
-    errors.add(:errors, "Corporation does not exist") unless @corp
+    errors.add(:errors, "Corporation does not exist") unless Corporation.exists?(@corp_id)
     errors.add(:errors, "You are already in a corporation, leave first") if @player.in_a_corporation?
     errors.empty?
   end
 
   def update_events
-    emit_event(PlayerJoinedCorporation, {player_id: @user.id, corporation_id: @corp.id})
+    emit_event(PlayerJoinedCorporation, {player_id: @player.id, corporation_id: @corp.id})
   end
 
   def call
     return nil unless validates?
+    @corp = Corporation.find(@corp_id)
     @player.update_attribute(:corporation_id, @corp.id)
+    update_events
+    @corp
   end
 
 end
