5b564e36bd568d7f93328a37c7a90b0ef8893d67
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 06:28:55 +0100

    checks for existence of corp, returns player now

--- app/services/leave_corporation_service.rb
+++ app/services/leave_corporation_service.rb
@@ -7,13 +7,13 @@ class LeaveCorporationService
 
   def initialize(user_id, corp_id)
     @player = Player.find(user_id)
-    @corp = Corporation.find(corp_id)
+    @corp_id = corp_id
     streams(["universe", "corporations", "corporation-#{corp_id}"])
   end
 
   def validates?
-    errors.add(:errors, "Corporation does not exist") unless @corp
-    errors.add(:errors, "You are not in this corporation") unless @player.in_corporation?(@corp.id)
+    errors.add(:errors, "Corporation does not exist") unless Corporation.exists?(@corp_id)
+    errors.add(:errors, "You are not in this corporation") unless @player.in_corporation?(@corp_id)
     errors.empty?
   end
 
@@ -24,6 +24,7 @@ class LeaveCorporationService
   def call
     return nil unless validates?
     @player.update_attribute(:corporation_id, nil)
+    @player
   end
 
 end
