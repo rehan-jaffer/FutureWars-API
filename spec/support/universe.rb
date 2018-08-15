3b2c21e9c0f49f627812e1012c84724f8d35a6ac
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 05:41:35 +0100

    create predictable mini universe for testing purposes

---
+++ spec/support/universe.rb
@@ -0,0 +1,39 @@
+def create_universe
+
+  Sector.destroy_all
+  Port.destroy_all
+  Warp.destroy_all
+
+  FactoryBot.create_list(:sector, 10)
+  FactoryBot.create(:special_port, sector_id: 1)
+  FactoryBot.create(:port, sector_id: 2, port_class: 3)
+  FactoryBot.create(:port, sector_id: 3, port_class: 3)
+  FactoryBot.create(:port, sector_id: 4, port_class: 5)
+  FactoryBot.create(:port, sector_id: 5, port_class: 6)
+  1.upto(10) do |i|
+    1.upto(10) do |j|
+      next if i == j
+      create_warp(i, j)
+    end
+  end
+  create_tunnel
+  create_hole
+end
+
+def create_tunnel
+  11.upto(14) do |i|
+    FactoryBot.create(:sector, id: i)
+  end
+
+  10.upto(14).to_a.each_cons(2) do |i, j|
+    create_warp(i, j)
+  end
+end
+
+def create_hole
+  FactoryBot.create(:sector, id: 15)
+end
+
+def create_warp(from, to)
+  Warp.create(origin_id: from, dest_id: to)
+end
