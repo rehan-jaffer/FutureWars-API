8fc5d88d48f67206e6e31460fe4e6b86c7b1dbb6
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 08:14:33 +0100

    reverted sector creation stage to old version, grid implementation still needs some work

--- lib/universe/stages/port_stage.rb
+++ lib/universe/stages/port_stage.rb
@@ -2,7 +2,8 @@ require './lib/port_trade'
 
 class PortStage
   def initialize
-    @ports = YAML.safe_load(File.read('./maps/sector.yml'))['Ports'].to_a
+#    @ports = YAML.safe_load(File.read('./maps/sector.yml'))['Ports'].to_a
+    @ports = []
     @port_dists = { 1 => 0.1999, 2 => 0.1991, 3 => 0.1999, 4 => 0.0997, 5 => 0.0997, 6 => 0.0997, 7 => 0.0496, 8 => 0.0522 }
     @sector_list = Sector.pluck(:id)
   end
@@ -12,7 +13,7 @@ class PortStage
   end
 
   def port_preset(port)
-    { port_class: port, sector_id: @sector_list.delete_at(@sector_list.length * rand) }
+    { "port_class": port, "sector_id": @sector_list.delete_at(@sector_list.length * rand) }
   end
 
   def port_distribution
@@ -24,7 +25,7 @@ class PortStage
   def exec
     port_distribution.each_with_index do |port, _i|
       sector_props = @ports.empty? ? port_preset(port) : @ports.pop[1]
-      PortCreatorService.call(sector_props)
+      PortCreatorService.call(sector_props).errors
     end
   end
 end
