c607247562b88f1a00f79e26b6db9943e0ba559d
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 05:41:53 +0100

    added required fields for port

--- spec/factories/port.rb
+++ spec/factories/port.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory :port do
     name 'Test Port'
-    port_class 2
+    port_class 1
+
+    ore_productivity 100
+    organics_productivity 100
+    equipment_productivity 100
+    ore_qty 100
+    organics_qty 100
+    equipment_qty 100
+
     factory :special_port do
       port_class 0
     end
