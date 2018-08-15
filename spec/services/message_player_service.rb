9a0ed98a76e813b0648055aa6d87ed5bf0734538
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 06:37:33 +0100

    added basic tests for message player service

---
+++ spec/services/message_player_service.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe MessagePlayerService do
+
+  describe "Messaging a Player that exists" do
+
+    before :all do
+      @player1 = FactoryBot.create(:player)
+      @player2 = FactoryBot.create(:player)
+    end
+
+    let(:message_player) { MessagePlayerService.call(@player1.id, @player2.username, "test message").result }
+
+    it "succeeds" do
+      expect(message_player).to be_a_kind_of Message
+    end
+
+  end
+
+  describe "Messaging a Player that doesn't exist" do
+
+    before :all do
+      @player1 = FactoryBot.create(:player)
+    end
+
+    let(:message_player) { MessagePlayerService.call(@player1.id, "bad name", "test message").errors }
+
+    it "succeeds" do
+      expect(message_player[:errors]).to include("No known trader with this name")
+    end
+
+  end
+
+end
