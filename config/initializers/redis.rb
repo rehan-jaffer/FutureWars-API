ecaa081408d43bf66c51b624f0d2c6da2ac897ed
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 08:11:23 +0100

    some changes made by rubocop

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 $redis = Redis.new
-#$redis_ns = Redis::Namespace.new(cnfg[:namespace], :redis => $redis) if cnfg[:namespace]
+# $redis_ns = Redis::Namespace.new(cnfg[:namespace], :redis => $redis) if cnfg[:namespace]
 
 # To clear out the db before each test
-$redis.flushdb if Rails.env = "test"
+$redis.flushdb if Rails.env == 'test'
+
