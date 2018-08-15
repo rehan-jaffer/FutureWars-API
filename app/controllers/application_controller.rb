e2de884911bddad946b26c0baf48bab7bf822e31
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 08:04:03 +0100

    change serialization_scope to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::API
   before_action :authenticate_request
   attr_reader :current_user
 
+  serialization_scope :current_user
+
   private
 
   def authenticate_request
