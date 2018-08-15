5b8ff194bd60d9c3855f0488f5a3fc2408d2c34e
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-15 08:29:12 +0100

    use where rather than find lookup

--- app/services/message_player_service.rb
+++ app/services/message_player_service.rb
@@ -4,12 +4,12 @@ class MessagePlayerService
 
   def initialize(from_id, to_user, message)
     @from_id = from_id
-    @to_id = Player.find_by_username(to_user).try(:id)
+    @to_id = Player.where(username: to_user).first.try(:id)
     @message = message
   end
 
   def validates?
-    errors.add(:errors, "No known trader with this name") if @to_id.nil?
+    errors.add(:errors, "No known trader with this name") unless @to_id
     errors.add(:errors, "Please specify a message") unless @message
     errors.empty?
   end
