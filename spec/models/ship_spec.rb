db769cb57778701705b3c383a58fcfffbe61757b
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-16 11:01:14 +0100

    initial implementation of ship spec

--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Ship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "hold info" do
+
+    before :all do
+      @player = FactoryBot.create(:player)
+      @ship = @player.primary_ship
+    end
+
+    describe "#total_holds" do
+     
+      it "returns the total number of holds" do
+        expect(@ship.total_holds).to eq 40
+      end
+
+    end
+
+  end
+
 end
