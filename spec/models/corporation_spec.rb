b8f6cf92b71daba79aba4b1529aa77c1dc97bf9d
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-16 06:46:53 +0100

    added very basic corporation specs

--- spec/models/corporation_spec.rb
+++ spec/models/corporation_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Corporation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:name) }
+
+  describe "with_ceo" do
+
+    before :all do
+      @player = FactoryBot.create(:player)
+      @corporation = FactoryBot.create(:corporation)
+      @corporation.ceo_id = @player.id
+      @corporation.save
+    end
+
+    it "should return corporations where the given player is CEO" do
+
+      expect(Corporation.with_ceo(@player.id).first).to eq @corporation
+  
+    end
+
+   end
+
 end
