821dbc1f2a093d51ec9d5381c0e7e0a22b22cae1
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-16 18:05:02 +0100

    fixed spec

--- spec/services/query_class_zero_items_service.rb
+++ spec/services/query_class_zero_items_service.rb
@@ -2,32 +2,32 @@ require 'rails_helper'
 
 describe QueryClassZeroItemsService do
 
-  describe "Player can query a class zero port and get item prices" do
+  describe "Querying a Class Zero Port" do
 
-    before :all do
-      @player = FactoryBot.create(:player)
-      @player.update_sector(1)
-    end
-
-    let (:class_zero_query) { QueryClassZeroItemsService.call(@player.id, 1).result }
-
-    it "succeeds" do
-      expect(class_zero_query).to have_key(:items)
-    end
-
+  before :each do
+    @player = FactoryBot.create(:player)
+    @player.update_sector(1)
   end
 
-  describe "Player cannot query a class zero port when not in the same sector" do
+    describe "Player can query a class zero port and get item prices" do
 
-    before :all do
-      @player = FactoryBot.create(:player)
-      @player.update_sector(2)
+      let (:class_zero_query) { QueryClassZeroItemsService.call(@player.id, 1).result }
+
+      it "succeeds" do
+        expect(class_zero_query).to have_key(:items)
+      end
+
     end
 
-    let (:class_zero_query) { QueryClassZeroItemsService.call(@player.id, 1).errors }
+    describe "Player cannot query a class zero port when not in the same sector" do
 
-    it "succeeds" do
-      expect(class_zero_query[:errors]).to include("You are not in the same sector as this port")
+      let (:class_zero_query) { QueryClassZeroItemsService.call(@player.id, 1).errors }
+
+      it "succeeds" do
+        @player.update_sector(2)
+        expect(class_zero_query[:errors]).to include("You are not in the same sector as this port")
+      end
+
     end
 
   end
