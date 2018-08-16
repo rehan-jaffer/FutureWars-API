46745f71537d52beb37ae945e54ea512321c55a4
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-16 06:47:28 +0100

    check for presence with name validation, not just uniqueness

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -3,7 +3,7 @@ class Corporation < ApplicationRecord
   has_one :ceo, foreign_key: "ceo_id", class_name: "Player"
   has_many :members, foreign_key: "corporation_id", class_name: "Player"
 
-  validates :name, uniqueness: { case_sensitive: true }
+  validates :name, uniqueness: { case_sensitive: true }, presence: true
 
   scope :with_ceo, -> (id) { where(ceo_id: id) }  
 
