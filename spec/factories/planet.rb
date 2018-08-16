cbeae728801eb950a8b4ba4b613b10fa25041e33
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-16 10:06:41 +0100

    added factories for planet and planet_type

---
+++ spec/factories/planet.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :planet do
+    name "Planetor Four"
+    planet_type_id 1
+  end
+end
---
+++ spec/factories/planet_type.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :planet_type do
+    id 1
+    name "Earth-type"
+    classification "M"
+  end
+end
