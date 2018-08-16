a4919a04a21abd70c13f3eec8c8d49745c3e98b0
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-16 10:06:12 +0100

    added additional tests of port creator service

--- spec/services/port_creator_service_spec.rb
+++ spec/services/port_creator_service_spec.rb
@@ -23,5 +23,24 @@ describe PortCreatorService do
         expect(result.errors).to have_key(:errors)
       end
     end
+
+    describe "sets up MCIC" do
+
+      let(:port) { PortCreatorService.call(sector_id: 2, port_class: 1).result }
+
+      it "has the MCIC set up for commodities" do
+        expect(port.ore_mcic).to be_between -90, 90
+        expect(port.organics_mcic).to be_between -75, 75
+        expect(port.equipment_mcic).to be_between -65, 65
+      end
+
+      it "sets up the initial qty to be ten times the productivity" do
+        expect(port.ore_qty).to eq port.ore_productivity*10
+        expect(port.organics_qty).to eq port.organics_productivity*10
+        expect(port.equipment_qty).to eq port.equipment_productivity*10
+      end
+
+    end
+
   end
 end
