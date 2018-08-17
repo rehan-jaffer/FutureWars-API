16191c213d813b14e41934bce52f93d8ca42f3b7
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-17 07:00:19 +0100

    refactored class to use class_zero methods from port, not player

--- app/services/purchase_class_zero_items_service.rb
+++ app/services/purchase_class_zero_items_service.rb
@@ -3,21 +3,21 @@ class PurchaseClassZeroItemsService
 
   def initialize(user_id, port_id, item, qty)
     @player = Player.where(id: user_id).first
-    @port = Port.where(id: port_id)
+    @port = Port.where(id: port_id).first
     @item = item
     @qty = qty
   end
 
   def validates?
-    errors.add(:errors, "You cannot buy that many") if @qty > @player.class_zero_costs[:items][@item.to_sym][:available]
-    errors.add(:errors, "Purchase not understood") unless [:fighters, :holds, :shields].include?(@item.to_sym)
+    errors.add(:errors, "You cannot buy that many") if @qty > @port.class_zero.item_count(@item.to_sym, @player.credits, @player.primary_ship)
+    errors.add(:errors, "Purchase not understood") unless class_zero_items.include?(@item.to_sym)
     errors.empty?
   end
 
   def call
     return nil unless validates?
     
-    cost = @qty * @player.class_zero_costs[:items][@item.to_sym][:cost]
+    cost = @qty * @port.class_zero.item_cost(@item.to_sym)
     @player.credits = @player.credits - cost
     ship = @player.primary_ship
     case @item.to_sym
@@ -26,9 +26,16 @@ class PurchaseClassZeroItemsService
       when :shields
         ship.shields += @qty
       when :holds
-        ship.holds += @qty
+        ship.additional_holds += @qty
     end
     ship.save && @player.save
     ship
   end
+
+  private
+
+    def class_zero_items
+      [:fighters, :holds, :shields]
+    end
+
 end
