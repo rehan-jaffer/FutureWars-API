327191290ac239b4eb6bd059a7b94d64e0b3c48c
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-17 07:00:56 +0100

    class zero port decorator

---
+++ app/models/class_zero_port.rb
@@ -0,0 +1,73 @@
+class ClassZeroPort
+
+  def initialize(port)
+    @port = port
+  end
+
+  def fighter_price
+    fighter_price = ClassZeroItems::Pricing.fighter_price
+  end
+
+  def shield_price
+    shield_price = ClassZeroItems::Pricing.shield_price
+  end
+
+  def next_hold_price
+    Holds.price(total_holds + 1)
+  end
+
+  def item_count(item, credits, ship)
+
+    return case item
+      when :fighters
+        fighter_count(credits, ship)
+      when :shields
+        shield_count(credits, ship)
+      when :holds
+        hold_count(credits, ship)
+    end
+
+  end
+
+  def item_cost(item)
+
+    return case item
+      when :fighters
+        fighter_price
+      when :shields
+        shield_price
+      when :holds
+        hold_count
+    end
+
+  end
+
+  def fighter_count(credits, ship)
+
+    fighter_count = [
+      credits / fighter_price,
+      ship.ship_type.max_fighters
+    ].min
+
+
+  end
+
+  def shield_count(credits, ship)
+
+    [
+      credits / shield_price,
+      ship.ship_type.max_shields
+    ].min
+
+  end
+
+  def hold_count(credits, ship)
+
+    ship.total_holds
+      .upto(ship_type.max_holds)
+      .map { |holds| Holds.price(holds) }
+      .find_index { |cost| cost >= credits }
+
+  end
+
+end
