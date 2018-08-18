a043193db42b74a22fb079cf9374e9d917caa361
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-18 13:37:58 +0100

    added more data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,41 +9,71 @@
 # Create Federation Space (zones 1-10)
 ShipType.destroy_all
 PlanetType.destroy_all
-#ShipType.create(name: "Merchant Freighter", turns_per_warp: 2, maximum_shields: 500, maximum_holds: 65, initial_holds: 30, maximum_fighters: 300)
-#ShipType.create(name: "Merchant Cruiser",turns_per_warp: 3, maximum_shields: 400, maximum_fighters: 2500, maximum_holds: 75, initial_holds: 20)
-#ShipType.create(name: "Cargo Tran", turns_per_warp: 4, maximum_shields: 1000, maximum_holds: 125, initial_holds: 50, maximum_fighters: 400)
+#ShipType.create(name: "Merchant Freighter", turns_per_warp: 2, max_shields: 500, max_holds: 65, initial_holds: 30, maximum_fighters: 300)
+#ShipType.create(name: "Merchant Cruiser",turns_per_warp: 3, max_shields: 400, maximum_fighters: 2500, max_holds: 75, initial_holds: 20)
+#ShipType.create(name: "Cargo Tran", turns_per_warp: 4, max_shields: 1000, max_holds: 125, initial_holds: 50, maximum_fighters: 400)
 ShipType.create(
     name: "Merchant Cruiser", turns_per_warp: 3, base_holds: 20, 
     min_holds: 20, max_fighters: 2500, max_shields: 400, max_holds: 75, 
-    max_fighters_per_attack: 750, max_fighters: 2500
+    max_fighters_per_attack: 750, max_fighters: 2500,
+    basic_hold_cost: 10000, main_drive_cost: 1000, computer_cost: 20300,
+    ship_hull_cost: 10000, ship_base_cost: 41300,
+    mine_max: 50, genesis_max: 5, transwarp_drive: false, max_transporter_range: 5,
+    offensive_odds: 10, beacon_max: 50, long_range_scanner: true, planet_scanner: true,
+    photon_missiles: false
   )
 ShipType.create(
     name: "Scout Marauder", turns_per_warp: 2, base_holds: 10, 
     min_holds: 10, max_fighters: 250, max_shields: 100, 
-    max_holds: 25, max_fighters_per_attack: 250, max_fighters: 250
+    max_holds: 25, max_fighters_per_attack: 250, max_fighters: 250,
+    basic_hold_cost: 5000, main_drive_cost: 3000, computer_cost: 5200,
+    ship_hull_cost: 2750, ship_base_cost: 15950,
+    mine_max: 0, genesis_max: 0, transwarp_drive: false, max_transporter_range: 0, 
+    offensive_odds: 20, beacon_max: 10, long_range_scan: true, planet_scanner: true,
+    photon_missiles: false
   )
 ShipType.create(
     name: "Merchant Freighter", turns_per_warp: 2, base_holds: 30, 
     min_holds: 30, max_fighters: 300, max_shields: 500, 
-    max_holds: 65, max_fighters_per_attack: 100, max_fighters: 300
+    max_holds: 65, max_fighters_per_attack: 100, max_fighters: 300,
+    basic_hold_cost: 15000, main_drive_cost: 2000, computer_cost: 9600,
+    ship_hull_cost: 6800, ship_base_cost: 33400,
+    mine_max: 0, genesis_max: 0, transwarp_drive: false, max_transporter_range: 0,
+    offensive_odds: 20, beacon_max: 10, long_range_scan: true, planet_scanner: true,
+    photon_missiles: false
   )
 ShipType.create(
     name: "Corporate Flagship", turns_per_warp: 3, base_holds: 20, 
     min_holds: 20, max_fighters: 20000, max_shields: 1500, 
     max_holds: 85, max_fighters_per_attack: 6000, 
-    max_fighters: 20000
+    max_fighters: 20000, mine_max: 100, genesis_max: 10, transwarp_drive: true,
+    max_transporter_range: 10, offensive_odds: 12, beacon_max: 100, long_range_scan: true,
+    planet_scanner: true, photon_missiles: false
   )
 ShipType.create(
     name: "CargoTran", turns_per_warp: 4, base_holds: 50,
     min_holds: 50, max_fighters: 400, max_shields: 1000,
-    max_holds: 125, max_fighters_per_attack: 125
+    max_holds: 125, max_fighters_per_attack: 125,
+    basic_hold_cost: 27000, main_drive_cost: 1000, computer_cost: 11050,
+    ship_hull_cost: 12900, ship_base_cost: 51950,
+    mine_max: 1, genesis_max: 2, transwarp_drive: false, offensive_odds: 8,
+    beacon_max: 20, long_range_scanner: true, planet_scanner: true,
+    photon_missiles: false
   )
 ShipType.create(
     name: "Corellian Battleship", turns_per_warp: 4, base_holds: 16,
     min_holds: 16, max_fighters: 10000, max_shields: 750,
-    max_holds: 80, max_fighters_per_attack: 3000
+    max_holds: 80, max_fighters_per_attack: 3000,
+    basic_hold_cost: 8000, main_drive_cost: 1000, computer_cost: 61500,
+    ship_hull_cost: 18000, ship_base_cost: 88500
   )
-ShipType.create(name: "Colonial Transport", turns_per_warp: 6)
+ShipType.create(name: "Colonial Transport", 
+   turns_per_warp: 6,
+   basic_hold_cost: 27000, main_drive_cost: 1000, computer_cost: 10400, ship_hull_cost: 25200,
+   ship_base_cost: 63600, min_holds: 50, max_fighters: 200, mine_max: 0, genesis_max: 5, transwarp_drive: false,
+   max_fighters_per_attack: 100, max_transporter_range: 7, max_holds: 250, max_shields: 500,
+   offensive_odds: 60, beacon_max: 10, long_range_scan: false, planet_scanner: true, photon_missiles: false)
+
 ShipType.create(name: "Missile Frigate", turns_per_warp: 3)
 ShipType.create(name: "Imperial Starship", turns_per_warp: 4)
 ShipType.create(name: "Havoc GunStar", turns_per_warp: 3)
