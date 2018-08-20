5e07a91a2f9c13e806763ca7dba873d549e7e630
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-20 10:31:52 +0100

    added product mixin, update_planet_products method for whenever to use in order to update the products on planets

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,7 +1,25 @@
+require 'pp'
+
 class Planet < ApplicationRecord
 
   belongs_to :planet_type
+  belongs_to :sector
+
+  validates :sector_id, presence: true
 
   include Mixins::Planet::Colonists
+  include Mixins::Planet::Products
+
+  def update_planet_products
+
+    commodities = Hash.new(0)
+
+    Port.commodities.each do |commodity|
+      commodities[commodity] = attributes[commodity] + estimated_hourly_product(commodity)
+    end
+
+    update(commodities)
+
+  end
 
 end
