98269ba4597b19049e02577ebd83df9372984353
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-20 07:24:46 +0100

    beginnings of new services

---
+++ app/services/assign_colonists_service.rb
@@ -0,0 +1,14 @@
+class AssignColonistsService
+
+  prepend SimpleCommand
+
+  def initialize(player_id, planet_id, commodity, colonists)
+    @player = Player.where(id: player_id).first
+    @planet = Planet.where(id: planet_id).first
+    @commodity = commodity
+    @colonists = colonists
+  end
+
+  def call
+  end
+end
---
+++ app/services/deploy_fighters_service.rb
@@ -0,0 +1,18 @@
+class DeployFightersService
+
+  prepend SimpleCommand
+
+  def initialize(player_id, fighter_count)
+    @player = Player.find(player_id)
+    @fighter_count = fighter_count
+  end
+
+  def validates?
+    errors.add(:errors, "You do not have that many fighters") if @fighter_count > 0 && @fighter_count > @player.primary_ship.fighters
+    errors.add(:errors, "Fighters not deployed") if @fighter_count < 0 && Sector.find(@player.current_sector).fighters < @fighter_count.abs
+  end
+
+  def call
+  end
+
+end
