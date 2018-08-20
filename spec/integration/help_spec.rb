ac6f589ba6759a67d9f228061d494e8832fc32d6
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-20 07:25:29 +0100

    added really basic tests for help-related functionality, currently just tests whether the ships API returns anything

---
+++ spec/integration/help_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require './spec/support/auth'
+
+describe "Help API" do
+
+  before :all do
+    @player = FactoryBot.create(:player)
+    @auth = authenticate_user('ray1', 'testpassword')  
+  end
+
+  describe "Ship Catalog" do
+
+    it "returns a list of ships" do
+      get '/api/ships', headers: { 'AUTHORIZATION': @auth['auth_token'] }
+      pp JSON.parse(response.body)
+    end
+
+  end
+
+end
