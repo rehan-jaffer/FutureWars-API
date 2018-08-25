759afd12252fb07d91f460af0daa1a8945e9039e
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-25 07:43:00 +0100

    added density scan service w/ policy object

---
+++ app/policy/density_scan_policy.rb
@@ -0,0 +1,25 @@
+class DensityScanPolicy
+
+  def initialize(user, sector)
+    @user = user
+    @sector = sector
+  end
+
+  def allowed?
+    conditions.all? { |c| c[1] }
+  end
+
+  def error
+    conditions.find { |c| !c[1] }[0]
+  end
+
+  private
+
+    def conditions
+      [
+        ["You don't have a density scanner", @user.primary_ship.has_equip?(:density_scanner)],
+        ["You are not in that sector", @user.current_sector == @sector.id]
+      ]
+    end
+
+end
---
+++ app/services/density_scan_service.rb
@@ -0,0 +1,25 @@
+class DensityScanService
+
+  prepend SimpleCommand
+
+  def initialize(current_user, sector_id)
+    @current_user = current_user
+    @sector = Sector.find(sector_id)    
+  end
+
+  def call
+
+    policy = DensityScanPolicy.new(@current_user, @sector)
+
+    unless policy.allowed?
+      errors.add(:errors, policy.error)
+      return nil
+    end
+
+    @sector.warps
+      .map { |warp| Sector.find(warp) }
+      .map { |warp| {id: warp.id, density: warp.density, anom: true} }
+
+  end
+
+end
