46fe4db9330ff8cce0cace2081c0575c310215a1
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-25 13:40:51 +0100

    added missing file required for travis-ci tests to run

---
+++ lib/mixins/ship/hardware.rb
@@ -0,0 +1,20 @@
+module Mixins
+  module Ship
+    module Hardware
+
+      def can_equip?(item)
+        true
+      end
+
+      def has_equip?(item)
+        true
+      end
+
+      def equip?
+
+      end
+
+    end
+  end
+end
+
