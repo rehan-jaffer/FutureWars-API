ede19839961657d80692bd2d7d6e88cce37ce6b0
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-25 14:50:06 +0100

    added distance method

--- lib/events/projections.rb
+++ lib/events/projections.rb
@@ -1,10 +1,11 @@
 module Projection
   module Player
-    def self.move_count(id)
+
+    def self.distance(id)
       RailsEventStore::Projection.from_stream("player_#{id}")
         .init( -> { { moves: 0 } })
         .when(PlayerMoved, ->(state, event) { state[:moves] += 1 })
-        .run(event_store)
+        .run(event_store)[:moves]
     end
 
     def self.explored_sectors(id)
