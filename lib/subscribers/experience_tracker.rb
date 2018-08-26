389bd3f2684b9a9eecb697db50e86b24c2622f7d
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-26 05:59:08 +0100

    rewrote to allow easier writing of event-based code

--- lib/subscribers/experience_tracker.rb
+++ lib/subscribers/experience_tracker.rb
@@ -1,21 +1,37 @@
 require './app/models/player'
 
-class ExperienceTracker
-  def call(event)
-    case event.type
-      when "PlayerMoved"
-        pid = event.data[:player_id]
-        Player.find(pid).increment!(:exp, 2)
-        event_store.publish(PlayerGainedExperience.new(data: {player_id: pid, exp: 2}), stream_name: "player-#{pid}")
-      else
-        puts event.type
-    end
+module EventHook
+  def publish(event_name, stream_name, data_fields)
+    event = Object.const_get(translate(event_name))
+    event_store.publish(event.new(data: data_fields), stream_name: stream_name)
   end
 
-  private
+  def on(event, event_name, &block)
+     yield event.data if translate(event_name) == event.type
+  end
 
-   def event_store
-     Rails.configuration.event_store
-   end
+  def translate(event_name_symbol)
+    event_name_symbol.to_s.split('_').map(&:capitalize).join('')
+  end
+
+  def event_store
+    Rails.configuration.event_store
+  end
+
+end
+
+class ExperienceTracker
+
+  include EventHook
+
+  def call(event)
+
+    on event, :player_moved do |data|
+      player = Player.find(data[:player_id])
+      player.increment!(:exp, 2)
+      publish :player_gained_experience, "player-#{player.id}", player_id: player.id, exp: 2
+    end
+
+  end
 
 end
