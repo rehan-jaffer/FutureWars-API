7513da2a9c1a3559d227b703ef99f1d6a9e10713
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-26 05:55:36 +0100

    refactored class to use policy object and new event emitting syntax

--- app/services/move_player_service.rb
+++ app/services/move_player_service.rb
@@ -8,31 +8,24 @@ class MovePlayerService
     @player = Player.find(player_id)
     @origin = @player.current_sector
     @dest = dest
-    streams(['universe', "player-#{@player.id}"])
-  end
-
-  def validates?
-    errors.add(:errors, 'These parts of space are not connected') unless Warp.connected?(@player.current_sector, @dest)
-    errors.add(:errors, 'You have no turns left') unless @player.turns > 0
-    errors.add(:errors, 'Not a valid sector ID') unless Sector.exists?(@dest)
-    errors.empty?
-  end
-
-  def update_player_state
-    @player.update_sector(@dest)
-    @player.decrease_turns(@player.move_cost(1))
-  end
-
-  def update_events
-    emit_event(PlayerMoved, player_id: @player.id,
-                            origin_id: @origin,
-                            dest_id: @dest)
+    streams2 @player
   end
 
   def call
-    return nil unless validates?
-    update_player_state
-    update_events
-    @player
+    policy = MovePlayerPolicy.new(@player, @dest)
+
+    if policy.allowed?
+      @player.update_sector(@dest)
+      @player.decrease_turns(@player.move_cost(1))
+      emit :player_moved, player_id: @player.id,
+                          origin_id: @origin,
+                          dest_id: @dest
+      return @player
+    end
+
+    if policy.denied?
+      errors.add(:errors, policy.error)
+      return nil
+    end
   end
 end
