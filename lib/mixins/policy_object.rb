7c968d5f3ca594a979f31cdb24a54c3e0a1ed62a
Rehan Jaffer <ray@thelondonvandal.com>
2018-08-26 05:58:11 +0100

    denied? syntactic sugar added, success/failure blocks allow evaluating code based on policy outcome

--- lib/mixins/policy_object.rb
+++ lib/mixins/policy_object.rb
@@ -3,7 +3,23 @@ module PolicyObject
     conditions.all? { |c| c[1] }
   end
 
+  def denied?
+    !allowed?
+  end
+
   def error
     conditions.find { |c| !c[1] }[0]
+  end
+
+  def success(&block)
+    if allowed?
+      yield
+    end
+  end
+
+  def failure(&block)
+    unless allowed?
+      yield error
+    end
   end
 end
