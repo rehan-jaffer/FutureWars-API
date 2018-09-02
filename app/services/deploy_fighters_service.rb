f83f3d5497b1cdf8c5f06b822416b3ac7e92a4c8
Rehan Jaffer <ray@thelondonvandal.com>
2018-09-02 08:23:05 +0100

    set mode and player id who deployed fighters

--- app/services/deploy_fighters_service.rb
+++ app/services/deploy_fighters_service.rb
@@ -1,9 +1,10 @@
 class DeployFightersService
   prepend SimpleCommand
 
-  def initialize(player, fighter_count)
+  def initialize(player, fighter_count, mode="defensive")
     @player = player
     @fighter_count = fighter_count
+    @mode = mode
   end
 
   def call
@@ -17,6 +18,8 @@ class DeployFightersService
       sector_id = @player.current_sector
       sector = Sector.find(sector_id)
       sector.fighters_deployed += @fighter_count
+      sector.fighters_player_id = @player.id
+      sector.fighters_mode = @mode
       @player.primary_ship.fighters += @fighter_count
       sector.save && @player.save
       return sector
